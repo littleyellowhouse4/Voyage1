9bc5899f13f41344ecf1d228e2a96b54c9bcf09e
Elliana Schmitt <eroseschmitt@gmail.com>
2020-07-29 10:46:19 -0400

    Initial Commit

---
+++ Voyage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Voyage
+//
+//  Created by Apple on 7/29/20.
+//  Copyright © 2020 Elliana Schmitt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
