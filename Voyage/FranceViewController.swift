4202f207b09e41dc0ee4c6715e027db528f38146
Elliana Schmitt <eroseschmitt@gmail.com>
2020-07-29 12:16:00 -0400

    initial commit

---
+++ Voyage/FranceViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FranceViewController.swift
+//  Voyage
+//
+//  Created by Apple on 7/29/20.
+//  Copyright © 2020 Elliana Schmitt. All rights reserved.
+//
+
+import UIKit
+
+class FranceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
